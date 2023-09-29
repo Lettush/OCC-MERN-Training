062805cb4b0d74ded381e02b531d28482be4780c
althani_leonida <althanimiguel@hotmail.com>
2023-09-29 15:53:54 +0800

    Leonida

---
+++ Leonida, Althani Miguel/Week 1/2 JavaScript/homepage.js
@@ -0,0 +1,141 @@
+// /*var age = 26;
+
+// var name = 'Shai Leonida';
+// //const name = 'Miguel';
+
+// console.log(age, name);
+// console.log(name[3]);
+// console.log(name.length);
+// console.log(name.toLowerCase);
+// console.log(name.indexOf('t'));s
+// console.log(name.lastIndexOf('i'));
+// console.log(name.slice(0,7));
+// console.log(name.substr(0,2));
+// console.log(name.replace('S','E'));
+
+
+// console.log(`My name is ${name} and I am ${age} years old.`);*/
+
+// let names = ['Shai', 'Migs', 'Eden'];
+
+// //names[1] = 'Miguel';
+// //console.log(names.push('Fullante'));
+
+// console.log(names.concat('Leonida','Guillena'));
+
+// console.log(names.pop());
+
+// let age=2;
+
+// //console.log(age, age+3, `the age is ${age}`);
+
+// let email = 'ario@example.com';
+// console.log(email.includes('@'));
+
+// console.log(email!='althani');
+// console.log(age<2);
+// console.log(age>2);
+// console.log(age<=2);
+// console.log(age>=2);
+
+// console.log(Number('100')+1);
+// console.log(String(20)+1);
+
+// //basic
+// for (let i=0; i<5; i++)
+// {
+//     console.log('in loop:', i);
+// }
+
+
+// //array
+// const Names =['shaun', 'mario', 'luigi'];
+
+// for(let i=0; i<Names.length; i++)
+// {
+//     console.log(Names[i]);
+// }
+
+// let i=3;
+
+// // while(i<Names.length)
+// // {
+// //     console.log(Names[i]);
+// //     i++;
+// // }
+// // do {
+// //     console.log(i);
+// //     i++;
+// // }while(i<3);
+
+// // const speak = function(name, time){
+// //     console.log(`good ${time}, ${name}`);
+// // }
+
+// // speak('Leonida','afternoon');
+
+// const calcArea=function(radius) {
+//     let area = 3.18 * radius;
+//     return area;
+// }
+
+// let AREA = calcArea(10);
+// console.log(AREA);
+
+// const calcArea = (radius) => 3.18 * radius**2;
+
+// let AREA = calcArea(10);
+// console.log(AREA)
+
+let people = ['leonida', 'althani', 'miguel'];
+
+// for(let i=0; i<people.length; i++)
+// {
+//     console.log(people[i]);
+// }
+
+// people.forEach(function(person)
+// {
+//     console.log(person);
+// }
+// )
+
+// const logPerson = (person, index) => { 
+// console.log(`${index} - heelo ${person}`);
+// }
+
+// people.forEach(logPerson);
+
+// let user = {
+//     name: 'crystal',
+//     age: 30,
+//     email: 'crystal@example.com',
+//     location: 'manila',
+//     blogs: [
+//         {title: 'Blog 1', likes: 20},
+//         {title: 'Blog 2', likes: 30}
+//     ],
+//     login: function() {
+//         console.log('the user is logged in');
+//     },
+//     logBlogs: function() {
+//         //console.log(this.blogs);
+//         console.log('this user has made these blogs:');
+//         this.blogs.forEach(blog =>
+//         {
+//             console.log(blog.title, blog.likes);
+//         }
+//         )
+//     }
+// };
+
+// // console.log(user.login());
+// console.log(user.logBlogs());
+
+console.log(Math.PI);
+
+console.log(Math.round(Math.PI));
+
+console.log(Math);
+
+console.log(Math.round(Math.random()*100))
