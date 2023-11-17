111b42e08e06a892b2bd692f8ec7cc458672a5e2
althani_leonida <althanimiguel@hotmail.com>
2023-11-17 13:53:57 +0800

    Added Delete Function

--- Leonida, Althani Miguel/Python_Training/django training/mysite/myapp/views.py
+++ Leonida, Althani Miguel/Python_Training/django training/mysite/myapp/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.utils import timezone
 from .models import Person
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .forms import PersonForm
 from .models import Person
@@ -48,6 +48,11 @@ class PersonUpdateView(UpdateView):
     form_class = PersonForm
     template_name = "myapp/person_update.html"
     success_url = "/"
+
+class PersonDeleteView(DeleteView):
+    model = Person
+    template_name = "myapp/person_delete.html"
+    success_url = "/" 
 
     
 """
--- Leonida, Althani Miguel/Python_Training/django training/mysite/mysite/urls.py
+++ Leonida, Althani Miguel/Python_Training/django training/mysite/mysite/urls.py
@@ -20,7 +20,7 @@ from myapp.views import PersonListView, PersonDetailView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.static import static
-from myapp.views import PersonListView, PersonDetailView, PersonCreateView, PersonUpdateView
+from myapp.views import PersonListView, PersonDetailView, PersonCreateView, PersonUpdateView, PersonDeleteView
 
 
 urlpatterns = [
@@ -28,7 +28,8 @@ urlpatterns = [
     path('', PersonListView.as_view(), name="person_list_view"),
     path('<int:pk>', PersonDetailView.as_view(), name="person_detail_view"),
     path('create/', PersonCreateView.as_view(), name="person_create_view"),
-    path('<int:pk>/update', PersonUpdateView.as_view(), name="person_update_view")
+    path('<int:pk>/update', PersonUpdateView.as_view(), name="person_update_view"),
+    path('<int:pk>/delete', PersonDeleteView.as_view(), name="person_delete_view")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
